f730a60ae527b83f00616c36812e6493616156df
Robert Allen <robby.allen24@gmail.com>
2016-04-07 17:32:52 -0400

    updated app icon

---
+++ KevinsSkiAdventure/KevinsSkiAdventure/GameScene.swift
@@ -0,0 +1,30 @@
+//
+//  GameScene.swift
+//  KevinsSkiAdventure
+//
+//  Created by Robert Allen on 4/3/16.
+//  Copyright (c) 2016 Robert Allen. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
